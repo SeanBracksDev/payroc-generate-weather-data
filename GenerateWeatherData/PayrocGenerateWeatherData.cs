fc4a0bfeb09e56bd75cff11b3da4f8eee5fbdff4
sean.brackenborough <sean.brackenborough@sysmech.co.uk>
2024-07-04 09:30:49 +0100

    Move weather entry data generation to it's own method

--- GenerateWeatherData/PayrocGenerateWeatherData.cs
+++ GenerateWeatherData/PayrocGenerateWeatherData.cs
@@ -2,6 +2,10 @@
 
 class PayrocGenerateWeatherData
 {
+    static readonly string[] Directions = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"];
+    const string TemperatureUnit = "C"; // TODO: Make this configurable - Celsius/Fahrenheit
+    const string WindSpeedUnit = "km/h";
+
     static void Main(string[] args)
     {
         // TODO: Dummy dates to test for now, replace with actual dates
@@ -15,28 +19,30 @@ class PayrocGenerateWeatherData
         Random random = new();
         return random.NextDouble() * (max - min) + min;
     }
+
+    public static string GenerateWeatherDataEntry()
+    {
+        double longitude = GenerateRandomDouble(-180, 180);
+        double latitude = GenerateRandomDouble(-90, 90);
+        double temperature = GenerateRandomDouble(-273, 273); // ? unclear what the max temp should be here
+        double windSpeed = GenerateRandomDouble(0, 400); // ? unclear what the max temp should be here
+        Random random = new();
+        string windDirection = Directions[random.Next(Directions.Length)];
+        int precipitation = (int)GenerateRandomDouble(0, 100);
+
+        return $"{longitude:F6}\t{latitude:F6}\t{temperature:F1}\t{TemperatureUnit}\t{windSpeed:F1}\t{WindSpeedUnit}\t{windDirection}\t{precipitation}"
+    }
+
     public static void GenerateWeatherData(DateTime startDatetime, DateTime endDatetime, string outputFilePath)
     {
-        string[] Directions = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"];
-        const string TemperatureUnit = "C"; // TODO: Make this configurable - Celsius/Fahrenheit
-        const string WindSpeedUnit = "km/h";
-
         DateTime currDatetime = startDatetime;
 
         using StreamWriter outputFile = new(outputFilePath);
 
         while (currDatetime < endDatetime)
         {
-            double longitude = GenerateRandomDouble(-180, 180);
-            double latitude = GenerateRandomDouble(-90, 90);
-            double temperature = GenerateRandomDouble(-273, 273); // ? unclear what the max temp should be here
-            double windSpeed = GenerateRandomDouble(0, 400); // ? unclear what the max temp should be here
-            Random random = new();
-            string windDirection = Directions[random.Next(Directions.Length)];
-            int precipitation = (int)GenerateRandomDouble(0, 100);
-
             outputFile.WriteLine($"{currDatetime.ToUniversalTime():yyyy-MM-dd HH:mmUTC}");
-            outputFile.WriteLine($"{longitude:F6}\t{latitude:F6}\t{temperature:F1}\t{TemperatureUnit}\t{windSpeed:F1}\t{WindSpeedUnit}\t{windDirection}\t{precipitation}");
+            outputFile.WriteLine();
 
             currDatetime = currDatetime.AddHours(1);
         }
