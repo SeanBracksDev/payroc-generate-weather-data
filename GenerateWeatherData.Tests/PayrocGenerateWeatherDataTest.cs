c3dfcc1ac745deba386df68ae2ffaadd4a7b4333
sean.brackenborough <sean.brackenborough@sysmech.co.uk>
2024-07-04 09:43:42 +0100

    test for GenerateWeatherDataEntry method

--- GenerateWeatherData.Tests/PayrocGenerateWeatherDataTest.cs
+++ GenerateWeatherData.Tests/PayrocGenerateWeatherDataTest.cs
@@ -12,5 +12,23 @@ namespace GenerateWeatherData.Tests
 
             Assert.InRange(result, min, max);
         }
+
+        [Fact]
+        public void TestGenerateWeatherDataEntry()
+        {
+            string weatherDataEntry = PayrocGenerateWeatherData.GenerateWeatherDataEntry();
+            string[] weatherDataEntryParts = weatherDataEntry.Split("\t");
+
+            Assert.Equal(8, weatherDataEntryParts.Length);
+            Assert.True(double.TryParse(weatherDataEntryParts[0], out double _));
+            Assert.True(double.TryParse(weatherDataEntryParts[1], out double _));
+            Assert.True(double.TryParse(weatherDataEntryParts[2], out double _));
+            Assert.Equal(PayrocGenerateWeatherData.TemperatureUnit, weatherDataEntryParts[3]);
+            Assert.True(double.TryParse(weatherDataEntryParts[4], out double _));
+            Assert.Equal(PayrocGenerateWeatherData.WindSpeedUnit, weatherDataEntryParts[5]);
+            Assert.Contains(weatherDataEntryParts[6], PayrocGenerateWeatherData.Directions);
+            Assert.True(int.TryParse(weatherDataEntryParts[7], out int _));
+        }
     }
+
 }
