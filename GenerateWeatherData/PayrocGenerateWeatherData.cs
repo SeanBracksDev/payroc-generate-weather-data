6b17d546b533f6fae41d97695588ce734648dd92
sean.brackenborough <sean.brackenborough@sysmech.co.uk>
2024-07-04 09:19:53 +0100

    Output datetimes for a specified time range to specified filepath

--- GenerateWeatherData/PayrocGenerateWeatherData.cs
+++ GenerateWeatherData/PayrocGenerateWeatherData.cs
@@ -4,6 +4,23 @@ class PayrocGenerateWeatherData
 {
     static void Main(string[] args)
     {
+        // TODO: Dummy dates to test for now, replace with actual dates
+        DateTime startDatetime = new(2024, 6, 1, 0, 0, 0, DateTimeKind.Utc);
+        DateTime endDatetime = startDatetime.AddDays(1);
+        GenerateWeatherData(startDatetime, endDatetime, "weather_data.wis");
+    }
+
+    public static void GenerateWeatherData(DateTime startDatetime, DateTime endDatetime, string outputFilePath)
+    {
+        DateTime currDatetime = startDatetime;
+
+        using StreamWriter outputFile = new(outputFilePath);
+
+        while (currDatetime < endDatetime)
+        {
+            outputFile.WriteLine($"{currDatetime.ToUniversalTime():yyyy-MM-dd HH:mmUTC}");
+            currDatetime = currDatetime.AddHours(1);
+        }
 
     }
 }
