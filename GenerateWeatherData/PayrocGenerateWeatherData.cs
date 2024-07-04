cb8b1ad05b863dd1dd5e04d25c1ba5d52d1448db
sean.brackenborough <sean.brackenborough@sysmech.co.uk>
2024-07-04 12:09:51 +0100

    load temperatureUnit from optional appsettings.json file (defaults to C if not provided)

--- GenerateWeatherData/PayrocGenerateWeatherData.cs
+++ GenerateWeatherData/PayrocGenerateWeatherData.cs
@@ -1,17 +1,42 @@
 ﻿namespace GenerateWeatherData;
 
+using Microsoft.Extensions.Configuration;
+
 public class PayrocGenerateWeatherData
 {
     public static readonly string[] Directions = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"];
-    public const string TemperatureUnit = "C"; // TODO: Make this configurable - Celsius/Fahrenheit
+    public static string? temperatureUnit; // TODO: Make this configurable - Celsius/Fahrenheit
     public const string WindSpeedUnit = "km/h";
-
     static void Main(string[] args)
     {
+        LoadConfiguration();
+
         // TODO: Dummy dates to test for now, replace with actual dates
         DateTime startDatetime = new(2024, 6, 1, 0, 0, 0, DateTimeKind.Utc);
         DateTime endDatetime = startDatetime.AddDays(1);
         GenerateWeatherDataFile(startDatetime, endDatetime, "weather_data.wis");
+    }
+
+    public static void LoadConfiguration()
+    {
+        IConfigurationRoot config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: true).Build();
+        temperatureUnit = config["AppSettings:TemperatureUnit"];
+
+        if (temperatureUnit == "Fahrenheit")
+        {
+            temperatureUnit = "F";
+        }
+        if (temperatureUnit == "Celsius")
+        {
+            temperatureUnit = "C";
+        }
+
+        // if no valud valid temperature unit provied or F/Fahrenheit not specified, default to Celsius
+        if (temperatureUnit != "F" && temperatureUnit != "C")
+        {
+            Console.WriteLine("Invalid temperature unit provided, defaulting to C (Celsius)");
+            temperatureUnit = "C";
+        }
     }
 
     public static double GenerateRandomDouble(double min, double max)
@@ -30,7 +55,7 @@ public class PayrocGenerateWeatherData
         string windDirection = Directions[random.Next(Directions.Length)];
         int precipitation = (int)GenerateRandomDouble(0, 100);
 
-        return $"{longitude:F6}\t{latitude:F6}\t{temperature:F1}\t{TemperatureUnit}\t{windSpeed:F1}\t{WindSpeedUnit}\t{windDirection}\t{precipitation}";
+        return $"{longitude:F6}\t{latitude:F6}\t{temperature:F1}\t{temperatureUnit}\t{windSpeed:F1}\t{WindSpeedUnit}\t{windDirection}\t{precipitation}";
     }
 
     public static void GenerateWeatherDataFile(DateTime startDatetime, DateTime endDatetime, string outputFilePath)
